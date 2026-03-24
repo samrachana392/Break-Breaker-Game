eb19399a840fbbd38f574aa80c3f84004c10c234
samrachana392 <samrachana302@gmail.com>
2026-03-24 17:31:13 +0545

    added input.h file

---
+++ input.h
@@ -0,0 +1,9 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+// ======================= INPUT HANDLERS ======================
+void keyboard(unsigned char key, int x, int y);
+void specialKeys(int key, int x, int y);
+void specialKeysUp(int key, int x, int y);
+
+#endif
